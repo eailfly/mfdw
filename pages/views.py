35389ece18a86fd9e4ad61ea7c877f50b3015f73
eailfly <eailfly@gmail.com>
2017-10-27 17:20:06 +0800

    add template

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Page
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("<h1>The Meandco Homepage</h1>")
+def index(request, pagename):
+    pagename = '/' + pagename
+    pg = Page.objects.get(permalink=pagename)
+    page_list = Page.objects.all()
+    context = {
+        'title': pg.title,
+        'content': pg.bodytext,
+        'last_updated': pg.update_date,
+        'page_list': page_list,
+    }
+    # assert False
+    return render(request, "pages/page.html", context)
